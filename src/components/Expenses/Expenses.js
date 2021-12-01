3b12404be55836ca847919172cc36b0c043a0eef
Alex Nguyen <hoangminh98@gmail.com>
2021-11-30 23:55:38 -0800

    Filter and Add Expense works now

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
@@ -12,30 +12,15 @@ const Expenses = (props) => {
         setSelectedYear(selectedYear);
     };
 
+    const filteredExpenses = props.expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === filterYear;
+    });
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filterYear} onSelectYear={applyYearFilter} />
-                <ExpenseItem
-                    title={props.expenses[0].title}
-                    amount={props.expenses[0].amount}
-                    date={props.expenses[0].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[1].title}
-                    amount={props.expenses[1].amount}
-                    date={props.expenses[1].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[2].title}
-                    amount={props.expenses[2].amount}
-                    date={props.expenses[2].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[3].title}
-                    amount={props.expenses[3].amount}
-                    date={props.expenses[3].date}
-                />
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     );
