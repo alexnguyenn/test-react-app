105e268380259d746b830408df192639710a64e6
Alex Nguyen <46719079+alexnguyenn@users.noreply.github.com>
2021-11-30 18:25:58 -0800

    prettier fix (#1)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,12 @@
 import "./Expenses.css";
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
-
     return (
         <Card className="expenses">
-            {props.expenses.map(expense => (
-                <ExpenseItem
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
+            {props.expenses.map((expense) => (
+                <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
             ))}
         </Card>
     );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -6,7 +6,7 @@ const NewExpense = () => {
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
-            id: Math.random().toString()
+            id: Math.random().toString(),
         };
     };
 
